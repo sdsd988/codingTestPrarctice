e76ba272138864564e99631f80c9f5fd09d4f7b6
JUNGSANGYUN <sdsd98987@gmail.com>
2023-10-19 02:23:25 +0900

    백준 stack java 문제 풀이

---
+++ practice/src/backjoon/B17298.java
@@ -0,0 +1,43 @@
+package backjoon;
+
+import java.io.*;
+import java.util.Stack;
+
+public class B17298 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] A = new int[n];
+        int[] ans = new int[n];
+
+        String[] str = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.valueOf(str[i]);
+        }
+
+        Stack<Integer> myStack = new Stack<>();
+        myStack.push(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!myStack.isEmpty() && A[myStack.peek()] < A[i]) {
+                ans[myStack.pop()] = A[i];
+            }
+            myStack.push(i);
+        }
+
+        while (!myStack.empty()) {
+            ans[myStack.pop()] = -1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            bw.write(ans[i] + " ");
+        }
+
+        bw.write("\n");
+        bw.flush();
+    }
+}
